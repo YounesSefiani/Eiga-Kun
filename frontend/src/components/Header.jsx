e37ea229906dbe1d3c06154ce247a168bbee0aa9
Younes SEFIANI <tokyotweener21@gmail.com>
2024-02-29 15:42:49 +0100

    Ajout du menu hamburger dans la navbar.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import EigaKunLogo from "../assets/EigaKunLogo.png";
 import "./Header.css";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [buttonOpen, setButtonOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+    setButtonOpen(!menuOpen);
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    setMenuOpen(false);
+  };
+
   return (
-    <div className="header">
-      <nav>
+    <div className={`header ${menuOpen ? "menu-open" : ""}`}>
+      <button
+        type="button"
+        className={`hamburger-button ${buttonOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
+        &#9776;
+      </button>
+      <nav className={menuOpen ? "menu-open" : ""}>
         <ul>
           <li>
             <Link to="/films">Films</Link>
@@ -27,6 +48,43 @@ function Header() {
           </li>
         </ul>
       </nav>
+      <div className={`hamburger-menu ${menuOpen ? "menu-open" : ""}`}>
+        <ul>
+          {isAuthenticated ? (
+            <>
+              <li>
+                <Link to="/profil">Mon profil</Link>
+              </li>
+              <li>
+                <Link to="/mes-films">Mes Films</Link>
+              </li>
+              <li>
+                <Link to="/mes-séries">Mes Séries</Link>
+              </li>
+              <li>
+                <button type="button" onClick={handleLogout}>
+                  Déconnexion
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/authentification">Connexion / Inscription</Link>
+              </li>
+              <li>
+                <Link to="/rechercher">Rechercher</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link to="/à-propos">A propos</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
       <main>
         <Outlet />
       </main>
