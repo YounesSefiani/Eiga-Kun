33b0696b3485d103f79f4e34aaab0378a48f50e5
Younes SEFIANI <tokyotweener21@gmail.com>
2024-03-03 20:35:57 +0100

    Installation de Axios et de React Bootstrap, et création des chemins films et films/:id et configuration des routes.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,39 @@
+// Main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import axios from "axios";
 import App from "./App";
+import MoviesPage from "./pages/MoviesPage";
+import OneMovie from "./components/OneMovie";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/films",
+    element: <MoviesPage />,
+    loader: () => {
+      return axios
+        .get(`${import.meta.env.VITE_BACKEND_URL}/api/movies`)
+        .then((res) => res.data)
+        .catch((err) => console.error(err));
+    },
+  },
+  {
+    path: "/films/:id",
+    element: <OneMovie />,
+    loader: async ({ params }) => {
+      return axios
+        .get(`${import.meta.env.VITE_BACKEND_URL}/api/movies/${params.id}`)
+        .then((res) => res.data)
+        .catch((err) => {
+          console.error(err);
+          return null;
+        });
+    },
   },
 ]);
 
