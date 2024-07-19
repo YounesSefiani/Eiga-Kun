1cb5caeea44086b137bb8b1bd375d4cb09f1a5a6
YounesSefiani <tokyotweener21@gmail.com>
2024-07-19 16:10:14 +0200

    Installation des fichiers controllers & managers pour la table movies, CRUD opérationnel

--- backend/src/router.js
+++ backend/src/router.js
@@ -20,4 +20,22 @@ router.post("/items", itemControllers.add);
 
 /* ************************************************************************* */
 
+// Import itemControllers module for handling item-related operations
+const moviesControllers = require("./controllers/moviesControllers");
+
+// Route to get a list of items
+router.get("/movies", moviesControllers.browse);
+
+// Route to get a specific item by ID
+router.get("/movies/:id", moviesControllers.read);
+
+// Route to add a new movie
+router.post("/movies", moviesControllers.add);
+
+// Route to update a movie
+router.put("/movies/:id", moviesControllers.edit);
+
+// Route to delete a movie
+router.delete("/movies/:id", moviesControllers.destroy);
+
 module.exports = router;
--- backend/src/tables.js
+++ backend/src/tables.js
@@ -4,9 +4,11 @@
 
 // Import the manager modules responsible for handling data operations on the tables
 const ItemManager = require("./models/ItemManager");
+const MovieManager = require("./models/MovieManager");
 
 const managers = [
   ItemManager,
+  MovieManager,
   // Add other managers here
 ];
 
