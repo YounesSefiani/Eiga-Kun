7f722861b22995ddaca8aba0e26ff770f4aabc55
YounesSefiani <tokyotweener21@gmail.com>
2024-07-29 17:47:08 +0200

    Personnalisation de la Page d'accueil (installation du menu carousel + arrière plan)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,64 @@
-import Counter from "./components/Counter";
-import logo from "./assets/logo.svg";
+import Carousel from "react-bootstrap/Carousel";
+import Header from "./components/Header/Header";
 
+import DeadPoolWolverine from "./assets/Carousel/Home/deadpoolwolverine.jpg";
+import TheBoysSeason4 from "./assets/Carousel/Home/theboysseason4.jpg";
+import Maxxxine from "./assets/Carousel/Home/maxine.jpg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React !</p>
-
-        <Counter />
-
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Header />
+      <div className="homeCarousel custom-carousel">
+        <Carousel data-bs-theme="dark">
+          <Carousel.Item>
+            <div className="top">
+              <h1>Actuellement au cinéma</h1>
+            </div>
+            <img
+              className="d-block w-100"
+              src={DeadPoolWolverine}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h5>Deadpool & Wolverine</h5>
+              <p>Deadpool est de retour, et il n'est pas le seul !</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="top">
+              <h1>Disponible sur Amazon Prime Vidéo</h1>
+            </div>
+            <img
+              className="d-block w-100"
+              src={TheBoysSeason4}
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h5>The Boys, saison 4</h5>
+              <p>
+                La saison 4 de The Boys est disponible, dans son intégralité sur
+                Amazon Prime Vidéo.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="top">
+              <h1>Prochainement aux cinémas !</h1>
+            </div>
+            <img className="d-block w-100" src={Maxxxine} alt="Third slide" />
+            <Carousel.Caption>
+              <h5>Maxxxine</h5>
+              <p>
+                Cette superstar vivra une horreur, et il ne s'agit pas d'un
+                film.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </div>
   );
 }
