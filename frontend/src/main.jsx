6375f8d2c20dc87c585b499cf10bebd0ee784c0a
YounesSefiani <tokyotweener21@gmail.com>
2024-08-09 19:35:43 +0200

    Téléchargement des librairies fortAwesome (pour les icones), axios, bootstrap (pour le carousel), react-simple-star-rating (pour l'attribution des notes) et react-toastify (pour les notifications)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import axios from "axios";
 
 import App from "./App";
+import MoviesPage from "./pages/Movies/MoviesPage/MoviesPage";
+import OneMoviePage from "./pages/Movies/OneMovie/OneMovie";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/films",
+    element: <MoviesPage />,
+    loader: () => {
+      return axios
+        .get(`${import.meta.env.VITE_BACKEND_URL}/api/movies`)
+        .then((res) => res.data)
+        .catch((err) => console.error(err));
+    },
+  },
+  {
+    path: "/films/:id",
+    element: <OneMoviePage />,
+    loader: async ({ params }) => {
+      return axios
+        .get(
+          `${import.meta.env.VITE_BACKEND_URL}/api/movies/${
+            params.id
+          }/movieCasting`
+        )
+        .then((res) => res.data)
+        .catch((err) => {
+          console.error(err);
+          return null;
+        });
+    },
   },
 ]);
 
