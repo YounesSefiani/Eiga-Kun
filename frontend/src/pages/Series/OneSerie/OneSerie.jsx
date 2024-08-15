3ed51f57caeac0462cd6ff42f5c6716e37104dc5
YounesSefiani <tokyotweener21@gmail.com>
2024-08-15 18:21:51 +0200

    Import des composants Seasons & Episodes

--- frontend/src/pages/Series/OneSerie/OneSerie.jsx
+++ frontend/src/pages/Series/OneSerie/OneSerie.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import Header from "../../../components/Header/Header";
+import Seasons from "../../../components/Seasons/Seasons";
+import SerieCasting from "../../../components/SerieCasting/SerieCasting";
 import FavoriteButtons from "../../../components/FavoriteButtons/FavoriteButtons";
-import "./OneSerie.css";
 import RatingFeedBack from "../../../components/RatingFeedBack/RatingFeedBack";
+import "./OneSerie.css";
 
 function OneSerie() {
   const serie = useLoaderData();
 
-  const zero = (number) => (number < 10 ? `0${number}` : number);
+  const formatDate = (date) => {
+    const zero = (number) => (number < 10 ? `0${number}` : number);
+    const d = new Date(date);
+    const day = zero(d.getDate());
+    const month = zero(d.getMonth() + 1);
+    const year = d.getFullYear();
+    return `${day}-${month}-${year}`;
+  };
 
-  const date = new Date(serie.release_date);
-  const day = zero(date.getDate());
-  const month = zero(date.getMonth() + 1);
-  const year = date.getFullYear();
-
-  const formattedDate = `${day}-${month}-${year}`;
+  const debutDate = formatDate(serie.release_date);
+  const endingDate = formatDate(serie.ending_date);
 
   return (
     <div className="oneSeriePage">
@@ -46,26 +51,28 @@ function OneSerie() {
               )}
             </div>
             <div className="oneSerieSeasons">
-              <p>
-                Première diffusion : <br />
-                {formattedDate}
-              </p>
-              <p>
-                {serie.seasons} <br />
-                saisons
-              </p>
-              <p>
-                {serie.episodes} <br />
-                épisodes
-              </p>
-              <p>
-                Dernière diffusion : <br />
-                {serie.ending_date}
-              </p>
-              <p>
-                Statut de la série : <br />
-                {serie.statut} au bout de {serie.seasons} saison(s)
-              </p>
+              <div className="oneSerieSeason">
+                <p>
+                  Première diffusion : <br />
+                  {debutDate}
+                </p>
+                <p>
+                  {serie.seasons.length} <br />
+                  saisons
+                </p>
+                <p>
+                  {serie.episodes} <br />
+                  épisodes
+                </p>
+                <p>
+                  Dernière diffusion : <br />
+                  {endingDate}
+                </p>
+                <p>
+                  Statut de la série : <br />
+                  {serie.statut} au bout de {serie.seasons.length} saison(s)
+                </p>
+              </div>
             </div>
             <div className="detailsAndSynopsisSerie">
               <div className="oneSerieDetails">
@@ -84,6 +91,12 @@ function OneSerie() {
             </div>
           </div>
         </div>
+      </div>
+      <div className="seasons">
+        <Seasons seasons={serie.seasons} />
+      </div>
+      <div className="serieCasting">
+        <SerieCasting serieCasting={serie.serieCasting} />
       </div>
       <div className="reviewsAndTrailer">
         <div className="ratingReviews">
