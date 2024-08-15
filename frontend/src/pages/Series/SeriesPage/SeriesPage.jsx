8ac8ac01260470b75560a2341738d3b129b2d727
YounesSefiani <tokyotweener21@gmail.com>
2024-08-15 14:13:17 +0200

    Ajout du Horizontal Scroller sur la page des séries, personnalisation de la page sur tout support.

--- frontend/src/pages/Series/SeriesPage/SeriesPage.jsx
+++ frontend/src/pages/Series/SeriesPage/SeriesPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import Header from "../../../components/Header/Header";
 import "./SeriesPage.css";
+import HorizontalScroll from "../../../components/HorizontalScroll/HorizontalScroll";
 
 function SeriesPage() {
   const series = useLoaderData();
@@ -12,17 +13,19 @@ function SeriesPage() {
       <div className="seriesList">
         <h1>Toutes les séries</h1>
         <div className="seriesContainer">
-          {series.map((serie) => (
-            <div className="serieCard">
-              <Link key={serie.id} to={`/séries/${serie.id}`}>
-                <img
-                  className="serieImg"
-                  src={serie.poster}
-                  alt={serie.title}
-                />
-              </Link>
-            </div>
-          ))}
+          <HorizontalScroll>
+            {series.map((serie) => (
+              <div className="serieCard">
+                <Link key={serie.id} to={`/séries/${serie.id}`}>
+                  <img
+                    className="serieImg"
+                    src={serie.poster}
+                    alt={serie.title}
+                  />
+                </Link>
+              </div>
+            ))}
+          </HorizontalScroll>
         </div>
       </div>
     </div>
