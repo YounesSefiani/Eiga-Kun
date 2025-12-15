c5b6d146947bbd249e34505872d61aca2a38fc87
YounesSefiani <younes.sefiani@outlook.fr>
2025-12-15 18:32:04 +0100

    Configuration CRUD / BREAD et initialisation des routes pour la table 'series'.

---
+++ backend/src/models/SerieManager.js
@@ -0,0 +1,98 @@
+const AbstractManager = require("./AbstractManager");
+
+class SerieManager extends AbstractManager {
+  constructor() {
+    super({ table: "series" });
+  }
+
+  // C - CRUD - Create
+  async createSerie(serie) {
+    const [serieCreated] = await this.database.query(
+      `INSERT INTO ${this.table} (title, poster, background, logo, trailer, synopsis, genre, theme, universe, subUniverse, beginning_date, ending_date, statut, nbSeasons, seasons, nbEpisodesSerie, episodes, duration, country, screen, streaming, original) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+      [
+        serie.title,
+        serie.poster,
+        serie.background,
+        serie.logo,
+        serie.trailer,
+        serie.synopsis,
+        serie.genre,
+        serie.theme,
+        serie.universe,
+        serie.subUniverse,
+        serie.beginning_date,
+        serie.ending_date,
+        serie.statut,
+        serie.nbSeasons,
+        serie.seasons,
+        serie.nbEpisodesSerie,
+        serie.episodes,
+        serie.duration,
+        serie.country,
+        serie.screen,
+        serie.streaming,
+        serie.original,
+      ]
+    );
+    return serieCreated;
+  }
+
+  // R - CRUD - Read
+  async readSeries() {
+    const [series] = await this.database.query(`SELECT * FROM ${this.table}`);
+    return series;
+  }
+
+  async readSerieId(id) {
+    const [serie] = await this.database.query(
+      `SELECT * FROM ${this.table} WHERE id = ?`,
+      [id]
+    );
+    return serie[0];
+  }
+
+  // U - CRUD - Update
+  async updateSerie(id, serie) {
+    const [updatedSerie] = await this.database.query(
+      `UPDATE ${this.table} SET title = ?, poster = ?, background = ?, logo = ?, trailer = ?, synopsis = ?, genre = ?, theme = ?, universe = ?, subUniverse = ?, beginning_date = ?, ending_date = ?, statut = ?, nbSeasons = ?, seasons = ?, nbEpisodesSerie = ?, episodes = ?, duration = ?, country = ?, screen = ?, streaming = ?, original = ? WHERE id = ?`,
+      [
+        serie.title,
+        serie.poster,
+        serie.background,
+        serie.logo,
+        serie.trailer,
+        serie.synopsis,
+        serie.genre,
+        serie.theme,
+        serie.universe,
+        serie.subUniverse,
+        serie.beginning_date,
+        serie.ending_date,
+        serie.statut,
+        serie.nbSeasons,
+        serie.seasons,
+        serie.nbEpisodesSerie,
+        serie.episodes,
+        serie.duration,
+        serie.country,
+        serie.screen,
+        serie.streaming,
+        serie.original,
+        id,
+      ]
+    );
+    return updatedSerie;
+  }
+
+  // D - CRUD - Delete
+
+  async deleteSerie(id) {
+    const [deletedSerie] = await this.database.query(
+      `DELETE FROM ${this.table} WHERE id = ?`,
+      [id]
+    );
+    return deletedSerie;
+  }
+}
+
+module.exports = SerieManager;
--- backend/src/router.js
+++ backend/src/router.js
@@ -11,4 +11,14 @@ router.post("/movies", moviesControllers.addMovie);
 router.put("/movies/:id", moviesControllers.editMovie);
 router.delete("/movies/:id", moviesControllers.destroyMovie);
 
+// SERIES // 
+
+const seriesControllers = require("./controllers/seriesControllers");
+
+router.get("/series", seriesControllers.browseSeries);
+router.get("/series/:id", seriesControllers.readOneSerie);
+router.post("/series", seriesControllers.addSerie);
+router.put("/series/:id", seriesControllers.editSerie);
+router.delete("/series/:id", seriesControllers.destroySerie);
+
 module.exports = router;
