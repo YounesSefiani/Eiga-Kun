dec1594d919e27bc5015af6a976762aa13d83f53
YounesSefiani <younes.sefiani@outlook.fr>
2026-01-08 18:29:05 +0100

    Modification Header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import EigaKunLogo from "../../assets/EigaKunLogo.png";
 import "./Header.css";
 
@@ -6,9 +7,9 @@ function Header() {
   return (
     <div className="header">
       {/* <h2>Header</h2> */}
-      <img src={EigaKunLogo} alt="Eiga-Kun Logo"/>
+      <Link to ="/"><img src={EigaKunLogo} alt="Eiga-Kun Logo"/></Link>
       <ul>
-        <li>Films</li>
+        <li><Link to="/films">Films</Link></li>
         <li>Séries</li>
         <li>Personnalités</li>
         <li>Blog</li>
