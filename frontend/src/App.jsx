327fea8e91b226459346e98c35ca1a0e38a08e01
YounesSefiani <younes.sefiani@outlook.fr>
2026-01-08 17:53:56 +0100

    Modification CSS du site afin que le header et le footer sur les verisons phones et tablets soient bien à leur place.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,47 +16,49 @@ function App() {
     setIndex(selectedIndex);
   };
   return (
-    <>
+    <div className="appContainer">
       <Header />
       <HeaderPhone />
-      <div className="homePage">
-        <Carousel activeIndex={index} onSelect={handleSelect}>
-          <Carousel.Item>
-            <img
-              src={AvengersDoomsdayCountdown}
-              alt="Avengers Doomsday Countdown"
-            />
-            <Carousel.Caption>
-              <h3>Le compte à rebours a commencé</h3>
-              <p>
-                Le prochain film Avengers : Doomsday sort le 16 Décembre 2026
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img src={TheBoysSeason5} alt="The Boys - Saison 5" />
-            <Carousel.Caption>
-              <h3>The Boys, la bataille finale</h3>
-              <p>
-                La cinquième et dernière saison de la série "The Boys" arrive
-                sur Amazon Prime Vidéo, le 8 Avril 2026.{" "}
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img src={Michael} alt="Michael" />
-            <Carousel.Caption>
-              <h3>Michael : La légende au cinéma</h3>
-              <p>
-                Découvrez le biopic du King of the Pop, Michael Jackson, au
-                cinéma le 22 Avril 2026.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </div>
+      <main className="mainContent">
+        <div className="homePage">
+          <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+              <img
+                src={AvengersDoomsdayCountdown}
+                alt="Avengers Doomsday Countdown"
+              />
+              <Carousel.Caption>
+                <h3>Le compte à rebours a commencé</h3>
+                <p>
+                  Le prochain film Avengers : Doomsday sort le 16 Décembre 2026
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={TheBoysSeason5} alt="The Boys - Saison 5" />
+              <Carousel.Caption>
+                <h3>The Boys, la bataille finale</h3>
+                <p>
+                  La cinquième et dernière saison de la série "The Boys" arrive
+                  sur Amazon Prime Vidéo, le 8 Avril 2026. {" "}
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={Michael} alt="Michael" />
+              <Carousel.Caption>
+                <h3>Michael : La légende au cinéma</h3>
+                <p>
+                  Découvrez le biopic du King of the Pop, Michael Jackson, au
+                  cinéma le 22 Avril 2026.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+      </main>
       <FooterPhone />
-    </>
+    </div>
   );
 }
 
