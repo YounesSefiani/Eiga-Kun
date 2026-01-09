80ccc97306f7d86e241478b736dfd25f656109ee
YounesSefiani <younes.sefiani@outlook.fr>
2026-01-09 18:44:12 +0100

    Création de la section des séries.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import React, { Children } from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import MoviesPage from './pages/Movies/MoviesPage/MoviesPage.jsx'
-import OneMoviePage from './pages/Movies/OneMoviePage/OneMoviePage.jsx'
+import React, { Children } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import MoviesPage from './pages/Movies/MoviesPage/MoviesPage.jsx';
+import OneMoviePage from './pages/Movies/OneMoviePage/OneMoviePage.jsx';
+import SeriesPage from './pages/Series/SeriesPage/SeriesPage.jsx';
+import OneSeriePage from './pages/Series/OneSeriePage/OneSeriePage.jsx';
 import connexion from "./services/connexion.js";
-import './index.css'
+import './index.css';
 
 
 import {
@@ -35,6 +37,30 @@ const router = createBrowserRouter([
     loader: async ({ params }) => {
       try {
         const res = await connexion.get(`/movies/${params.id}`);
+        return res.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+  {
+    path: "/series",
+    element: <SeriesPage />,
+    loader: async () => {
+      try {
+        const res = await connexion.get('/series');
+        return res.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+  {
+    path: "/series/:id",
+    element: <OneSeriePage />,
+    loader: async ({ params }) => {
+      try {
+        const res = await connexion.get(`/series/${params.id}/full`);
         return res.data;
       } catch (error) {
         console.error(error);
