4235597417eacfd805c97f2647990173a6cee631
YounesSefiani <younes.sefiani@outlook.fr>
2026-01-15 17:39:51 +0100

    Importation du Casting.

--- frontend/src/components/CastingFilmography/CastingContainer/CastingContainer.jsx
+++ frontend/src/components/CastingFilmography/CastingContainer/CastingContainer.jsx
@@ -20,34 +20,34 @@ function CastingContainer({ casting }) {
             <HorizontalScroll>
               {realisation.map((cast) => (
                 <div className="realisationCard" key={cast.id}>
-                    <Link to={`/personalities/${cast.personality_id}`}>
-                  {cast.personality_picture ? (
-                    <img
-                      src={
-                        cast.personality_picture &&
-                        cast.personality_picture.startsWith("http")
-                          ? cast.personality_picture
-                          : cast.personality_picture
-                          ? `http://localhost:3994/src/assets/Personalities/Pictures/${cast.personality_picture}`
-                          : ""
-                      }
-                      alt={cast.personalityName}
-                      className="realisationPicture"
-                    />
-                  ) : (
-                    <div className="realisationPictureHolder">
-                      <FontAwesomeIcon icon={faStar} />
-                      <p>Aucune image pour le moment.</p>
-                    </div>
-                  )}
-                  <h4>{cast.personality_fullname}</h4>
-                  <p>
-                    <b>Rôle :</b>
-                    <br />
-                    {cast.role}
-                  </p>
-                  {cast.presence ? <p>{cast.presence}</p> : null}
-                    </Link>
+                  <Link to={`/personalities/${cast.personality_id}`}>
+                    {cast.personality_picture ? (
+                      <img
+                        src={
+                          cast.personality_picture &&
+                          cast.personality_picture.startsWith("http")
+                            ? cast.personality_picture
+                            : cast.personality_picture
+                            ? `http://localhost:3994/src/assets/Personalities/Pictures/${cast.personality_picture}`
+                            : ""
+                        }
+                        alt={cast.personalityName}
+                        className="realisationPicture"
+                      />
+                    ) : (
+                      <div className="realisationPictureHolder">
+                        <FontAwesomeIcon icon={faStar} />
+                        <p>Aucune image pour le moment.</p>
+                      </div>
+                    )}
+                    <h4>{cast.personality_fullname}</h4>
+                    <p>
+                      <b>Rôle :</b>
+                      <br />
+                      {cast.role}
+                    </p>
+                    {cast.presence ? <p>{cast.presence}</p> : null}
+                  </Link>
                 </div>
               ))}
             </HorizontalScroll>
@@ -89,7 +89,13 @@ function CastingContainer({ casting }) {
                       <br />
                       {cast.role}
                     </p>
-                    {cast.presence ? <p>{cast.presence}</p> : null}
+                    {cast.presence ? (
+                      <p>
+                        <b>Présence :</b>
+                        <br />
+                        {cast.presence}
+                      </p>
+                    ) : null}
                   </Link>
                 </div>
               ))}
